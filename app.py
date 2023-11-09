399bd880ada21860c3ad2dc81cde5401d86b9c1c
Rafael Ribeiro <rafael.amaral.ribeiro@gmail.com>
2023-11-09 12:42:50 -0300

    Updated with Price x Type Histogram

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
+import plotly.graph_objects as go
 
 car_data = pd.read_csv('vehicles.csv')  # lendo os dados
+
+# Separando a informação de modelo e fabricante
+car_data[['manufacturer', 'model']
+         ] = car_data['model'].str.split(' ', 1, expand=True)
 
 st.header('Data Viewer')
 st.dataframe(car_data)
 
 st.header('Vehicle Types by Manufacturer')
-# Separando a informação de modelo e fabricante
-car_data[['manufacturer', 'model']
-         ] = car_data['model'].str.split(' ', 1, expand=True)
+
 # Agrupando os dados por Fabricante
 group_manufacturers = car_data.groupby(['manufacturer', 'type'])[
     'date_posted'].count()
@@ -40,3 +43,34 @@ if hist_button:  # se o botão for clicado
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
+
+st.header("Compare price distribuition between types of car")
+
+# Tipos de carros que serão usados na comparação
+car_types = car_data['type'].unique()
+type = st.selectbox("Select the fisrt type of car", car_types)
+type2 = st.selectbox("Select the second type of car", car_types, index=1)
+
+# Checkbox para normalizar o gráfico
+normalized = st.checkbox("Normalize histogram", value=True)
+if normalized:
+    histnorm = 'probability'
+    yaxis_title_text = 'Percent'
+else:
+    histnorm = ''
+    yaxis_title_text = 'Count'
+
+# Filtrando Dataframe com os tipos selecionados
+car_data_filtered = car_data[car_data['type'] == type]
+car_data_filtered2 = car_data[car_data['type'] == type2]
+
+# Construindo Histograma Preço X Tipo
+hist_type = go.Figure()
+hist_type.add_trace(go.Histogram(
+    x=car_data_filtered['price'], name=type, histnorm=histnorm))
+hist_type.add_trace(go.Histogram(
+    x=car_data_filtered2['price'], name=type2, histnorm=histnorm))
+hist_type.update_layout(
+    barmode='overlay', xaxis_title_text='Price', yaxis_title_text=yaxis_title_text)
+hist_type.update_traces(opacity=0.6)
+st.plotly_chart(hist_type, use_container_width=True)
