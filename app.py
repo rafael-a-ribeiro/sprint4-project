9ee20bc35a193c6982f9d3d34c470c649d26e365
Rafael Ribeiro <rafael.amaral.ribeiro@gmail.com>
2023-11-08 22:49:18 -0300

    Updated with Stacked Bar Chart

--- app.py
+++ app.py
@@ -6,6 +6,23 @@ car_data = pd.read_csv('vehicles.csv')  # lendo os dados
 
 st.header('Data Viewer')
 st.dataframe(car_data)
+
+st.header('Vehicle Types by Manufacturer')
+# Separando a informação de modelo e fabricante
+car_data[['manufacturer', 'model']
+         ] = car_data['model'].str.split(' ', 1, expand=True)
+# Agrupando os dados por Fabricante
+group_manufacturers = car_data.groupby(['manufacturer', 'type'])[
+    'date_posted'].count()
+group_manufacturers = group_manufacturers.reset_index()
+# Trocando o nome da coluna para ficar de acordo com a informação guardada
+group_manufacturers.rename(columns={'date_posted': 'count'}, inplace=True)
+# Gerando o gráfico de barra
+bar_chart = px.bar(group_manufacturers, x='manufacturer',
+                   y='count', color='type', barmode='stack')
+st.plotly_chart(bar_chart, use_container_width=True)
+# fig.show()
+
 hist_button = st.button('Criar histograma')  # criar um botão
 
 if hist_button:  # se o botão for clicado
